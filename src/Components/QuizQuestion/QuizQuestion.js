ab4979760b32745c3e365a0ae4886d50b2d3607f
max <promitms7@gmail.com>
2022-10-11 13:56:02 +0600

    icon funtioallity added

--- src/Components/QuizQuestion/QuizQuestion.js
+++ src/Components/QuizQuestion/QuizQuestion.js
@@ -3,6 +3,7 @@ import Option from '../Option/Option';
 import './QuizQuestion.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AiFillEye } from "react-icons/ai";
 
 const QuizQuestion = ({singlequestion}) => {
     const {question,options,correctAnswer} = singlequestion;
@@ -14,10 +15,15 @@ const QuizQuestion = ({singlequestion}) => {
             toast('Wrogn Answer Mate')
         }
     }
-
+    const handlerIcon = ()=>{
+        toast(correctAnswer)
+    }
     return (
         <div className='quiz_cart'>
             <h5 className='m-3'>{question}</h5>
+                <div>
+                <AiFillEye onClick={handlerIcon}></AiFillEye>
+                </div>
             {
                 options.map(option => <Option hadlerQuizChecker={hadlerQuizChecker} option={option}></Option>)
             }
